fb5813daae244cc0b577a610a84622ed3e2476e6
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-01-18 23:30:57 +0100

    Feedback input completed

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import Button from "./shared/Button"
+import Card from "./shared/Card"
+
+
+function FeedbackForm() {
+    const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
+    const [btnDisabled, setBtnDisabled] = useState('true')
+    const [message, setMessage] = useState('')
+
+
+
+    const handleTextChange = (e)=>{
+       if(text === ''){
+           setBtnDisabled(true)
+           setMessage(null)
+       } else if(text !== '' && text.trim().length<=10){
+           setMessage('Text must be at least 10 characters')
+           setBtnDisabled(true)
+       } else{
+           setMessage(null)
+           setBtnDisabled(false)
+       }
+       setText(e.target.value)
+    }
+    
+    return (
+        <Card>
+            <form>
+                <h2>How will you rate your service with us?</h2>
+                {/* { @todo - rating select component} */}
+                <div className="input-group">
+                    <input type="text" placeholder="Write a reveiw" onChange={handleTextChange} value={text} />
+                    <Button type="submit" isDisabled={btnDisabled}>
+                        Send
+                        </Button>
+                </div>
+
+                {message && <div className="message">{message}</div>}
+            </form>
+        </Card>
+    )
+}
+
+export default FeedbackForm
